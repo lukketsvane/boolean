c6f80e39c7994829189f06f6fe253100092dfe46
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2024-11-06 23:01:45 +0100

    Update project and deploy to Vercel

--- 3d-primitive-intersection/app/layout.tsx
+++ 3d-primitive-intersection/app/layout.tsx
@@ -15,8 +15,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full">
+      <body className={`${inter.className} h-full`}>
+        <main className="h-full w-full">
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
